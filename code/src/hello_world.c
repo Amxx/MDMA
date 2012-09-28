ae57388560bb2e0fbe5e56f6496cb073236721a6
Henri <henri.derycke@ens-lyon.fr>
2012-09-28 17:59:35 +0200

    Sans le using namespace std :)

--- code/src/hello_world.c
+++ code/src/hello_world.c
@@ -1,9 +1,9 @@
 #include <iostream>
 
-using namespace std;
+//using namespace std;
 
 int main(int argc, char* argv[])
 {
-	cout << "Hello World" << endl;
-	return 0;
+  std::cout << "Hello World" << std::endl;
+  return 0;
 }
