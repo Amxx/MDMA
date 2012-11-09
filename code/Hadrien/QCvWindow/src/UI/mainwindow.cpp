7815d20d22a7c3ec6986314781fce89cda12c3c3
Hadrien Croubois <hadrien.croubois@ens-lyon.fr>
2012-11-09 12:35:11 +0100

    debug

--- code/Hadrien/QCvWindow/src/UI/mainwindow.cpp
+++ code/Hadrien/QCvWindow/src/UI/mainwindow.cpp
@@ -7,39 +7,63 @@
  *	###############################################################################################
  */
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) :
+	QMainWindow(parent),
+	ui(new Ui::MainWindow),
+	config(ui, this),
+	camera_manager(config, this),
+	zone_manager(config, this)
 {
 	ui->setupUi(this);
-
-	config = new configuration(ui);
-	cameraDisplayer = new cvCameraDisplay(config, this);
-	zoneDisplayer = new zoneDisplay(config, this);
-
 	ui->statusbar->showMessage("Ready");
-}
-
-void MainWindow::mousePressEvent(QMouseEvent* ev)
-{
-	switch(ev->button())
-	{
-		case Qt::LeftButton:
-		{
-			int x = ev->x()-ui->label_camera->x();
-			int y = ev->y()-ui->label_camera->y()-ui->menubar->size().height();
-			if(x>=0 && x<=640 && y>=0 && y<=480)
-			{
-				zoneDisplayer->clic(QPoint(x, y));
-			}
-			break;
-		}
-		default:
-			break;
-	}
 }
 
 MainWindow::~MainWindow()
 {
 	delete ui;
+}
+
+void MainWindow::mousePressEvent(QMouseEvent* ev)
+{
+	int x = ev->x()-ui->label_camera->x();
+	int y = ev->y()-ui->label_camera->y()-ui->menubar->size().height();
+
+	if(!config.running && x>=0 && x<=640 && y>=0 && y<=480)
+		switch(ev->button())
+		{
+			case Qt::LeftButton:
+				zone_manager.clic(QPoint(x, y));
+				break;
+			case Qt::RightButton:
+				zone_manager.reset_clic();
+				break;
+			default:
+				break;
+		}
+}
+
+void MainWindow::closeEvent(QCloseEvent* ev)
+{
+	if(config.changed)
+	{
+		switch(QMessageBox::question(this, "Changed have been made to the configuration", "Would you like to save changes made to \""+config.name+"\" before closing ?", QMessageBox::Yes | QMessageBox::No | QMessageBox::Cancel))
+		{
+			case QMessageBox::Yes:
+				if(config.save())
+					ev->accept();
+				else
+					ev->ignore();
+				break;
+			case QMessageBox::No:
+				ev->accept();
+				break;
+			default:
+				ev->ignore();
+				break;
+		}
+	}
+	else
+		ev->accept();
 }
 
 /*
@@ -48,50 +72,104 @@ MainWindow::~MainWindow()
  *	###############################################################################################
  */
 
+void MainWindow::on_actionNew_triggered()
+{
+	if(!config.running)
+	{
+		config.reset();
+		setWindowTitle("MDMA - Motion Detection Midi Interface - "+config.name);
+	}
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+	if(!config.running)
+	{
+		config.open();
+		setWindowTitle("MDMA - Motion Detection Midi Interface - "+config.name);
+	}
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+	if(!config.running)
+	{
+		config.save();
+		setWindowTitle("MDMA - Motion Detection Midi Interface - "+config.name);
+	}
+}
+
+void MainWindow::on_actionSave_As_triggered()
+{
+	if(!config.running)
+	{
+		config.saveas();
+		setWindowTitle("MDMA - Motion Detection Midi Interface - "+config.name);
+	}
+}
+
+void MainWindow::on_actionAbout_MDMA_triggered()
+{
+	if(!config.running)
+	{
+	}
+}
+
+void MainWindow::on_actionAbout_Qt_triggered()
+{
+	if(!config.running)
+	{
+		qApp->aboutQt();
+	}
+}
+
+void MainWindow::on_pushButton_run_clicked()
+{
+	if(config.running)
+	{
+		config.running = false;
+		ui_disable(false);
+		ui->pushButton_run->setText("Run");
+		ui->statusbar->showMessage("Ready");
+	}
+	else
+	{
+		config.running = true;
+		zone_manager.reset_clic();
+		ui_disable(true);
+		ui->pushButton_run->setText("Stop");
+		ui->statusbar->showMessage("Running");
+	}
+}
+
 void MainWindow::on_pushButton_configure_pressed()
 {
 	configwindow configW;
 	configW.exec();
 }
 
-void MainWindow::on_actionAbout_Qt_triggered()
-{
-	qApp->aboutQt();
-}
-
-void MainWindow::on_pushButton_run_clicked()
-{
-	if(ui->pushButton_run->text() == "Run")
-	{
-		ui->pushButton_run->setText("Stop");
-		ui_disable(true);
-		ui->statusbar->showMessage("Running");
-	}
-	else
-	{
-		ui->pushButton_run->setText("Run");
-		ui_disable(false);
-		ui->statusbar->showMessage("Ready");
-	}
-}
-
-
 void MainWindow::on_pushButton_edit_clicked()
 {
 	if(ui->treeWidget_list->currentItem() != NULL)
 	{
 		QString name = ui->treeWidget_list->currentItem()->text(0);
-		eventZone evz = config->zones[name];
-		zoneEditor popup(&evz);
+		eventZone& evz = config.zones[name];
+		zoneEditor popup(evz);
 
 		if(popup.exec())
 		{
-//			delete config->zones.find(name);
-//			config->zones.insert(evz.name, evz);
+			if(name != evz.name)
+			{
+				eventZone evz_t(config.zones[name]);
+				config.zones.remove(name);
+				config.zones.insert(evz_t.name, evz_t);
+			}
 
-//			ui->treeWidget_list->currentItem()->setText(0, evz.name);
-//			ui->treeWidget_list->currentItem()->setText(1, MDMA::type_to_string(evz.type));
-//			ui->treeWidget_list->currentItem()->setText(2, QString::number(evz.tab));
+			ui->treeWidget_list->currentItem()->setText(0, evz.name);
+			ui->treeWidget_list->currentItem()->setText(1, MDMA::type_to_string(evz.type));
+			ui->treeWidget_list->currentItem()->setText(2, QString::number(evz.tab + 1));
+
+			config.changed = true;
 		}
 	}
 }
@@ -101,8 +179,9 @@ void MainWindow::on_pushButton_delete_clicked()
 {
 	if(ui->treeWidget_list->currentItem() != NULL)
 	{
-		config->zones.erase(config->zones.find(ui->treeWidget_list->currentItem()->text(0)));
+		config.zones.erase(config.zones.find(ui->treeWidget_list->currentItem()->text(0)));
 		delete ui->treeWidget_list->currentItem();
+		config.changed = true;
 	}
 }
 
@@ -112,10 +191,16 @@ void MainWindow::on_pushButton_deleteAll_clicked()
 	if(QMessageBox::question(this, "Delete all", "Are you sure ?", QMessageBox::Yes | QMessageBox::No) == QMessageBox::Yes)
 	{
 		ui->treeWidget_list->clear();
-		config->zones.clear();
+		config.zones.clear();
+		config.changed = true;
 	}
 }
 
+void MainWindow::on_comboBox_tab_currentIndexChanged(int index)
+{
+	config.current_tab = index;
+	config.changed = true;
+}
 
 /*
  *	###############################################################################################
@@ -132,3 +217,4 @@ void MainWindow::ui_disable(bool b)
 	ui->pushButton_deleteAll->setDisabled(b);
 	ui->pushButton_edit->setDisabled(b);
 }
+
--- code/Hadrien/QCvWindow/src/UI/zoneeditor.cpp
+++ code/Hadrien/QCvWindow/src/UI/zoneeditor.cpp
@@ -1,26 +1,49 @@
 #include "zoneeditor.h"
 #include "ui_zoneeditor.h"
 
-zoneEditor::zoneEditor(eventZone* _evz, QWidget *parent) :
+zoneEditor::zoneEditor(eventZone& _evz, QWidget *parent) :
 	QDialog(parent),
 	ui(new Ui::zoneEditor),
 	evz(_evz)
 {
 	ui->setupUi(this);
 
-	ui->lineEdit_name->setText(evz->name);
+	ui->lineEdit_name->setText(evz.name);
+	ui->lineEdit_x1->setText(QString::number(evz.P1.x()));
+	ui->lineEdit_y1->setText(QString::number(evz.P1.y()));
+	ui->lineEdit_x2->setText(QString::number(evz.P2.x()));
+	ui->lineEdit_y2->setText(QString::number(evz.P2.y()));
+	ui->comboBox_tab->setCurrentIndex(evz.tab);
 
-	ui->lineEdit_x1->setText(QString::number(evz->P1.x()));
-	ui->lineEdit_y1->setText(QString::number(evz->P1.y()));
-	ui->lineEdit_x2->setText(QString::number(evz->P2.x()));
-	ui->lineEdit_y2->setText(QString::number(evz->P2.y()));
+	ui->tabWidget_type->setCurrentIndex(evz.type);
 
-	ui->comboBox_tab->setCurrentIndex(evz->tab);
+	ui->comboBox_axex_0->setCurrentIndex(evz.active[MDMA::EVENT_X]);
+	ui->spinBox_axex_1->setValue(evz.signal[MDMA::EVENT_X][0] & 15);
+	ui->spinBox_axex_2->setValue(evz.signal[MDMA::EVENT_X][1] & 127);
+	ui->comboBox_axey_0->setCurrentIndex(evz.active[MDMA::EVENT_Y]);
+	ui->spinBox_axey_1->setValue(evz.signal[MDMA::EVENT_Y][0] & 15);
+	ui->spinBox_axey_2->setValue(evz.signal[MDMA::EVENT_Y][1] & 127);
 
-	ui->tabWidget_type->setCurrentIndex(evz->type);
-
-	ui->comboBox_axes->setCurrentIndex(evz->axes);
-	on_comboBox_axes_currentIndexChanged(evz->axes);
+	ui->comboBox_enter->setCurrentIndex(evz.active[MDMA::ENTER]);
+	ui->spinBox_enter_1->setValue(evz.signal[MDMA::ENTER][0] & 15);
+	ui->spinBox_enter_2->setValue(evz.signal[MDMA::ENTER][1] & 127);
+	ui->spinBox_enter_3->setValue(evz.signal[MDMA::ENTER][2] & 127);
+	ui->comboBox_exit->setCurrentIndex(evz.active[MDMA::EXIT]);
+	ui->spinBox_exit_1->setValue(evz.signal[MDMA::EXIT][0] & 15);
+	ui->spinBox_exit_2->setValue(evz.signal[MDMA::EXIT][1] & 127);
+	ui->spinBox_exit_3->setValue(evz.signal[MDMA::EXIT][2] & 127);
+	ui->comboBox_open->setCurrentIndex(evz.active[MDMA::OPEN]);
+	ui->spinBox_open_1->setValue(evz.signal[MDMA::OPEN][0] & 15);
+	ui->spinBox_open_2->setValue(evz.signal[MDMA::OPEN][1] & 127);
+	ui->spinBox_open_3->setValue(evz.signal[MDMA::OPEN][2] & 127);
+	ui->comboBox_close->setCurrentIndex(evz.active[MDMA::CLOSE]);
+	ui->spinBox_close_1->setValue(evz.signal[MDMA::CLOSE][0] & 15);
+	ui->spinBox_close_2->setValue(evz.signal[MDMA::CLOSE][1] & 127);
+	ui->spinBox_close_3->setValue(evz.signal[MDMA::CLOSE][2] & 127);
+	ui->comboBox_shock->setCurrentIndex(evz.active[MDMA::SHOCK]);
+	ui->spinBox_shock_1->setValue(evz.signal[MDMA::SHOCK][0] & 15);
+	ui->spinBox_shock_2->setValue(evz.signal[MDMA::SHOCK][1] & 127);
+	ui->spinBox_shock_3->setValue(evz.signal[MDMA::SHOCK][2] & 127);
 }
 
 zoneEditor::~zoneEditor()
@@ -28,48 +51,159 @@ zoneEditor::~zoneEditor()
 	delete ui;
 }
 
-
-
-
-
-void zoneEditor::on_comboBox_axes_currentIndexChanged(int index)
-{
-	switch(index)
-	{
-		case MDMA::AXE_X:
-			ui->lineEdit_axex_1->setDisabled(false);
-			ui->lineEdit_axex_2->setDisabled(false);
-			ui->lineEdit_axey_1->setDisabled(true);
-			ui->lineEdit_axey_2->setDisabled(true);
-			break;
-		case MDMA::AXE_Y:
-			ui->lineEdit_axex_1->setDisabled(true);
-			ui->lineEdit_axex_2->setDisabled(true);
-			ui->lineEdit_axey_1->setDisabled(false);
-			ui->lineEdit_axey_2->setDisabled(false);
-			break;
-		case MDMA::AXE_XY:
-			ui->lineEdit_axex_1->setDisabled(false);
-			ui->lineEdit_axex_2->setDisabled(false);
-			ui->lineEdit_axey_1->setDisabled(false);
-			ui->lineEdit_axey_2->setDisabled(false);
-			break;
-
-	}
-}
+// ========================================================================================
 
 void zoneEditor::on_buttonBox_accepted()
 {
 
-	evz->name = ui->lineEdit_name->text();
-	evz->P1 = QPoint(ui->lineEdit_x1->text().toInt(), ui->lineEdit_y1->text().toInt());
-	evz->P2 = QPoint(ui->lineEdit_x2->text().toInt(), ui->lineEdit_y2->text().toInt());
-	evz->tab = ui->comboBox_tab->currentIndex();
+	evz.name = ui->lineEdit_name->text();
+	evz.P1 = QPoint(ui->lineEdit_x1->text().toInt(), ui->lineEdit_y1->text().toInt());
+	evz.P2 = QPoint(ui->lineEdit_x2->text().toInt(), ui->lineEdit_y2->text().toInt());
+	evz.tab = ui->comboBox_tab->currentIndex();
 
-	evz->type = MDMA::type(ui->tabWidget_type->currentIndex());
-	evz->axes = MDMA::axes(ui->comboBox_axes->currentIndex());
+	evz.type = MDMA::type(ui->tabWidget_type->currentIndex());
 
-//	char events[7][3];
+	evz.active[MDMA::EVENT_X] = (MDMA::active) (ui->comboBox_axex_0->currentIndex());
+	evz.signal[MDMA::EVENT_X][0] = ((MDMA::is_midi(evz.active[MDMA::EVENT_X]))?(evz.active[MDMA::EVENT_X]-1)<<4:0) | ui->spinBox_axex_1->value();
+	evz.signal[MDMA::EVENT_X][1] = 128 | ui->spinBox_axex_2->value();
+	evz.active[MDMA::EVENT_Y] = (MDMA::active) (ui->comboBox_axey_0->currentIndex());
+	evz.signal[MDMA::EVENT_Y][0] = ((MDMA::is_midi(evz.active[MDMA::EVENT_Y]))?(evz.active[MDMA::EVENT_Y]-1)<<4:0) | ui->spinBox_axey_1->value();
+	evz.signal[MDMA::EVENT_Y][1] = 128 | ui->spinBox_axey_2->value();
+
+	evz.active[MDMA::ENTER] = (MDMA::active) ui->comboBox_enter->currentIndex();
+	evz.signal[MDMA::ENTER][0] = ((MDMA::is_midi(evz.active[MDMA::ENTER]))?(evz.active[MDMA::ENTER]-1)<<4:0) | ui->spinBox_enter_1->value();
+	evz.signal[MDMA::ENTER][1] = 128 | ui->spinBox_enter_2->value();
+	evz.signal[MDMA::ENTER][2] = 128 | ui->spinBox_enter_3->value();
+	evz.active[MDMA::EXIT] = (MDMA::active) ui->comboBox_exit->currentIndex();
+	evz.signal[MDMA::EXIT][0] = ((MDMA::is_midi(evz.active[MDMA::EXIT]))?(evz.active[MDMA::EXIT]-1)<<4:0) | ui->spinBox_exit_1->value();
+	evz.signal[MDMA::EXIT][1] = 128 | ui->spinBox_exit_2->value();
+	evz.signal[MDMA::EXIT][2] = 128 | ui->spinBox_exit_3->value();
+	evz.active[MDMA::OPEN] = (MDMA::active) ui->comboBox_open->currentIndex();
+	evz.signal[MDMA::OPEN][0] = ((MDMA::is_midi(evz.active[MDMA::OPEN]))?(evz.active[MDMA::OPEN]-1)<<4:0) | ui->spinBox_open_1->value();
+	evz.signal[MDMA::OPEN][1] = 128 | ui->spinBox_open_2->value();
+	evz.signal[MDMA::OPEN][2] = 128 | ui->spinBox_open_3->value();
+	evz.active[MDMA::CLOSE] = (MDMA::active) ui->comboBox_close->currentIndex();
+	evz.signal[MDMA::CLOSE][0] = ((MDMA::is_midi(evz.active[MDMA::CLOSE]))?(evz.active[MDMA::CLOSE]-1)<<4:0) | ui->spinBox_close_1->value();
+	evz.signal[MDMA::CLOSE][1] = 128 | ui->spinBox_close_2->value();
+	evz.signal[MDMA::CLOSE][2] = 128 | ui->spinBox_close_3->value();
+	evz.active[MDMA::SHOCK] = (MDMA::active) ui->comboBox_shock->currentIndex();
+	evz.signal[MDMA::SHOCK][0] = ((MDMA::is_midi(evz.active[MDMA::SHOCK]))?(evz.active[MDMA::SHOCK]-1)<<4:0) | ui->spinBox_shock_1->value();
+	evz.signal[MDMA::SHOCK][1] = 128 | ui->spinBox_shock_2->value();
+	evz.signal[MDMA::SHOCK][2] = 128 | ui->spinBox_shock_3->value();
 
 	accept();
 }
+
+// ========================================================================================
+
+
+void zoneEditor::on_comboBox_axex_0_currentIndexChanged(int index)
+{
+	if(MDMA::is_midi((MDMA::active) index))
+	{
+		ui->spinBox_axex_1->setDisabled(false);
+		ui->spinBox_axex_2->setDisabled(false);
+	}
+	else
+	{
+		ui->spinBox_axex_1->setDisabled(true);
+		ui->spinBox_axex_2->setDisabled(true);
+	}
+}
+
+void zoneEditor::on_comboBox_axey_0_currentIndexChanged(int index)
+{
+	if(MDMA::is_midi((MDMA::active) index))
+	{
+		ui->spinBox_axey_1->setDisabled(false);
+		ui->spinBox_axey_2->setDisabled(false);
+	}
+	else
+	{
+		ui->spinBox_axey_1->setDisabled(true);
+		ui->spinBox_axey_2->setDisabled(true);
+	}
+}
+
+// ========================================================================================
+
+void zoneEditor::on_comboBox_enter_currentIndexChanged(int index)
+{
+	if(MDMA::is_midi((MDMA::active) index))
+	{
+		ui->spinBox_enter_1->setDisabled(false);
+		ui->spinBox_enter_2->setDisabled(false);
+		ui->spinBox_enter_3->setDisabled(false);
+	}
+	else
+	{
+		ui->spinBox_enter_1->setDisabled(true);
+		ui->spinBox_enter_2->setDisabled(true);
+		ui->spinBox_enter_3->setDisabled(true);
+	}
+}
+
+void zoneEditor::on_comboBox_exit_currentIndexChanged(int index)
+{
+	if(MDMA::is_midi((MDMA::active) index))
+	{
+		ui->spinBox_exit_1->setDisabled(false);
+		ui->spinBox_exit_2->setDisabled(false);
+		ui->spinBox_exit_3->setDisabled(false);
+	}
+	else
+	{
+		ui->spinBox_exit_1->setDisabled(true);
+		ui->spinBox_exit_2->setDisabled(true);
+		ui->spinBox_exit_3->setDisabled(true);
+	}
+}
+
+void zoneEditor::on_comboBox_open_currentIndexChanged(int index)
+{
+	if(MDMA::is_midi((MDMA::active) index))
+	{
+		ui->spinBox_open_1->setDisabled(false);
+		ui->spinBox_open_2->setDisabled(false);
+		ui->spinBox_open_3->setDisabled(false);
+	}
+	else
+	{
+		ui->spinBox_open_1->setDisabled(true);
+		ui->spinBox_open_2->setDisabled(true);
+		ui->spinBox_open_3->setDisabled(true);
+	}
+}
+
+void zoneEditor::on_comboBox_close_currentIndexChanged(int index)
+{
+	if(MDMA::is_midi((MDMA::active) index))
+	{
+		ui->spinBox_close_1->setDisabled(false);
+		ui->spinBox_close_2->setDisabled(false);
+		ui->spinBox_close_3->setDisabled(false);
+	}
+	else
+	{
+		ui->spinBox_close_1->setDisabled(true);
+		ui->spinBox_close_2->setDisabled(true);
+		ui->spinBox_close_3->setDisabled(true);
+	}
+}
+
+void zoneEditor::on_comboBox_shock_currentIndexChanged(int index)
+{
+	if(MDMA::is_midi((MDMA::active) index))
+	{
+		ui->spinBox_shock_1->setDisabled(false);
+		ui->spinBox_shock_2->setDisabled(false);
+		ui->spinBox_shock_3->setDisabled(false);
+	}
+	else
+	{
+		ui->spinBox_shock_1->setDisabled(true);
+		ui->spinBox_shock_2->setDisabled(true);
+		ui->spinBox_shock_3->setDisabled(true);
+	}
+}
+
