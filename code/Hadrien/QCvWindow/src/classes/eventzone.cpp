08f6c3649a4afeb3d4e11876005029f56c4d1bad
Hadrien Croubois <hadrien.croubois@ens-lyon.fr>
2012-11-09 12:34:21 +0100

    debug

--- code/Hadrien/QCvWindow/src/classes/eventzone.cpp
+++ code/Hadrien/QCvWindow/src/classes/eventzone.cpp
@@ -4,11 +4,52 @@ eventZone::eventZone()
 {
 }
 
-eventZone::eventZone(QPoint _P1, QPoint _P2, int _tab) : P1(_P1), P2(_P2), tab(_tab)
+
+eventZone::eventZone(const eventZone &cpy) :
+	name(cpy.name),
+	P1(cpy.P1),
+	P2(cpy.P2),
+	tab(cpy.tab),
+	type(cpy.type)
 {
-	name = "New Zone";
-	type = MDMA::FADER;
-	axes = MDMA::AXE_X;
+	for(int i=0; i<7; i++)
+	{
+		active[i] = cpy.active[i];
+		for(int j=0; j<3; j++)
+			signal[i][j] = cpy.signal[i][j];
+	}
+}
+
+
+eventZone::eventZone(QPoint _P1, QPoint _P2, int _tab) :
+	name("New Zone"),
+	P1(_P1),
+	P2(_P2),
+	tab(_tab),
+	type(MDMA::FADER)
+{
+	for(int i=0; i<7; i++)
+	{
+		switch((MDMA::event)i)
+		{
+			case MDMA::EVENT_X:
+			case MDMA::EVENT_Y:
+				active[i] = MDMA::NOTHING;
+				signal[i][0] = 0;
+				signal[i][1] = 0;
+				signal[i][2] = 196;
+				break;
+			case MDMA::ENTER:
+			case MDMA::EXIT:
+			case MDMA::OPEN:
+			case MDMA::CLOSE:
+			case MDMA::SHOCK:
+				active[i] = MDMA::NOTHING;
+				signal[i][0] = 0;
+				signal[i][1] = 0;
+				signal[i][2] = 0;
+		}
+	}
 }
 
 eventZone::~eventZone()
@@ -16,14 +57,33 @@ eventZone::~eventZone()
 }
 
 
-
-QString MDMA::type_to_string(type t)
+void eventZone::display(QPainter& painter)
 {
-	switch(t)
+	QFont font;
+	font.setPointSize(8);
+	painter.setFont(font);
+
+	switch(type)
 	{
-		case FADER : return "Fader";
-		case PAD : return "Pad";
-		case SEGMENT : return "Segment";
+		case MDMA::FADER :
+			painter.fillRect(QRect(P1, P2), MDMA::type_to_color(type));
+
+			painter.setPen(MDMA::text_color);
+			painter.drawText(std::min(P1.x(), P2.x())+2, std::min(P1.y(), P2.y())+10, name);
+			break;
+		case MDMA::PAD :
+			painter.fillRect(QRect(P1, P2), MDMA::type_to_color(type));
+
+			painter.setPen(MDMA::text_color);
+			painter.drawText(std::min(P1.x(), P2.x())+2, std::min(P1.y(), P2.y())+10, name);
+			break;
+		case MDMA::SEGMENT :
+			painter.setPen(QPen(QBrush(MDMA::type_to_color(type)), 4, Qt::SolidLine, Qt::RoundCap));
+			painter.drawLine(P1, P2);
+
+			painter.setPen(MDMA::text_color);
+			painter.drawText((P1.y()<P2.y() || P1.x()<P2.x())?P1:P2, name);
+			break;
 	}
-	return "";
 }
+
--- code/Hadrien/QCvWindow/src/classes/eventzone.h
+++ code/Hadrien/QCvWindow/src/classes/eventzone.h
@@ -2,44 +2,23 @@
 #define EVENTZONE_H
 
 #include <QMap>
+#include <QPainter>
 #include <QPoint>
 #include <QString>
 
-#include "midisignal.h"
-
-namespace MDMA
-{
-	enum type
-	{
-		FADER,
-		PAD,
-		SEGMENT
-	};
-	enum axes
-	{
-		AXE_X,
-		AXE_Y,
-		AXE_XY
-	};
-	enum event
-	{
-		EVENT_X,
-		EVENT_Y,
-		ENTER,
-		EXIT,
-		OPEN,
-		CLOSE,
-		SHOCK
-	};
-	QString type_to_string(type t);
-}
+#include "../config/constant.h"
 
 class eventZone
 {
 	public:
-		explicit eventZone();
-		explicit eventZone(QPoint _P1, QPoint _P2, int _tab);
+		eventZone();
+		eventZone(const eventZone &cpy);
+		eventZone(QPoint _P1, QPoint _P2, int _tab);
 		~eventZone();
+
+		void display(QPainter& painter);
+
+
 
 		QString name;
 		QPoint P1;
@@ -47,9 +26,9 @@ class eventZone
 		int tab;
 
 		MDMA::type type;
-		MDMA::axes axes;
 
-		char events[7][3];
+		MDMA::active active[7];
+		MDMA::signal signal[7];
 
 };
 
