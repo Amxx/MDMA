905039697ab4308a3bf6a9daea89f3614e1f298f
vanTot <timothee.bernard@ens-lyon.fr>
2012-11-19 11:42:26 +0100

    nettoyage code midi

--- code/Hadrien/QCvWindow/src/objects/midimanager.cpp
+++ code/Hadrien/QCvWindow/src/objects/midimanager.cpp
@@ -9,7 +9,13 @@ MidiManager::~MidiManager() throw()
 
 void MidiManager::createPort(QString name)
 {
+#ifndef WIN32
 	openVirtualPort(name.toStdString());
+#endif
+
+#ifdef WIN32
+	// Go VirtuamMIDISDK
+#endif
 }
 
 void MidiManager::changePort(int n)
@@ -23,7 +29,7 @@ void MidiManager::sendMessage(const unsigned char* msg)
 	
 	if(((msg[0] & 0xf0) == 0xc0) || ((msg[0] & 0xf0) == 0xd0))//program change or channel aftertouch
 	{
-		qDebug() << "size 2 : program change or  channel aftertouch";
+		qDebug() << "size 2 : program change or channel aftertouch";
 		v.assign(msg, msg+2);
 	}
 	else
--- code/midi/mdma.cpp
+++ code/midi/mdma.cpp
@@ -43,6 +43,52 @@ void send3(RtMidiOut *out, unsigned char a, unsigned char b, unsigned char c)
 	out->sendMessage(&msg);
 }
 
+void displayOutputPorts(RtMidiOut *midiout) {
+	int nPorts = midiout->getPortCount();
+	std::cout << "\nThere are " << nPorts << " MIDI output ports available.\n";
+
+	for(int i=0; i<nPorts; i++) {
+	    std::string portName = midiout->getPortName(i);
+	    std::cout << "    Output Port #" << i+1 << ": " << portName << std::endl;
+	}
+}
+
+int askOutputPorts(RtMidiOut *midiout) {
+	int nPorts = midiout->getPortCount();
+	std::cout << "\nThere are " << nPorts << " MIDI output ports available.\n";
+
+	for(int i = 0; i < nPorts; i++) {
+	    std::string portName = midiout->getPortName(i);
+	    std::cout << "    Output Port #" << i+1 << ": " << portName << std::endl;
+	}
+
+#ifndef WIN32
+	std::cout << "    Output Port #" << nPorts+1 << ": Create virtual port" << std::endl;
+#endif
+
+	int portNumber = 0;
+
+	while(portNumber == 0) {
+		cout << "Choose a MIDI output port.\n";
+		cin >> portNumber;
+
+#ifndef WIN32
+		if(portNumber == nPorts+1) {
+			return -1;
+		}
+#endif
+
+		if(portNumber < 1 || portNumber > nPorts) {
+		    cout << "Invalid port number.\n\n";
+		    portNumber = 0;
+		}
+	}
+
+	portNumber--;
+
+	return portNumber;
+}
+
 int main()
 {
 	// Create an api map. (to print api names more easily)
@@ -58,196 +104,45 @@ int main()
 	RtMidi :: getCompiledApi( apis );
 
 	std::cout << "\nCompiled APIs:\n";
-	for ( unsigned int i=0; i<apis.size(); i++ )
-	std::cout << "    " << apiMap[ apis[i] ] << std::endl;
+	for(unsigned int i=0; i < apis.size(); i++) {
+		std::cout << "    " << apiMap[ apis[i] ] << std::endl;
+	}
 
 	RtMidiOut *midiout = 0;
 
 	try {
-	// RtMidiOut constructor ... exception possible
-	midiout = new RtMidiOut();
+		// RtMidiOut constructor ... exception possible
+		midiout = new RtMidiOut();
 
-	std::cout << "\nCurrent output API: " << apiMap[ midiout->getCurrentApi() ] << std::endl;
-	
-	// Check outputs.
-	int nPorts = midiout->getPortCount();
-	std::cout << "\nThere are " << nPorts << " MIDI output ports available.\n";
-
-	for ( int i=0; i<nPorts; i++ ) {
-	    std::string portName = midiout->getPortName(i);
-	    std::cout << "    Output Port #" << i+1 << ": " << portName << std::endl;
+		std::cout << "\nCurrent output API: " << apiMap[ midiout->getCurrentApi() ] << std::endl;
+	} catch (RtError &error) {
+		error.printMessage();
+		return 1;
 	}
-	#ifndef WIN32
-	std::cout << "    Output Port #" << nPorts+1 << ": Create virtual port" << std::endl;
-	#endif
-	std::cout << std::endl;
-
-	} catch ( RtError &error ) {
-	error.printMessage();
-	return 1;
-	}
-
 
 	std::vector<unsigned char> message;
 
-	// Check available ports.
-	int nPorts = midiout->getPortCount();
-	if ( nPorts == 0 ) {
-	std::cout << "No ports available!\n";
-	delete midiout;
-	return 1;
-	}
-	int portNumber = -1;
+	int portNumber = askOutputPorts(midiout);
+	std::string portName = "MDMA";
 
-	while(portNumber == -1) {
-	cout << "Choose a MIDI output port.\n";
-	cin >> portNumber;
-	
-	if(portNumber < 1 || portNumber > nPorts
-	#ifndef WIN32
-	+1
-	#endif
-	) {
-	    cout << "Invalid port number.\n\n";
-	    portNumber = -1;
+	if(portNumber == -1) {
+		midiout->openVirtualPort(portName);
+//		std::cout << "Création d'un port virtuel" << std::endl;
 	}
-	}
-
-	portNumber--;
-#ifndef WIN32
-	if(portNumber == nPorts)
-	{
-		midiout->openVirtualPort("Test Virtual Port");
-	}
-	else
-	{
-#endif
+	else {
 		// Open first available port.
 		midiout->openPort(portNumber);
-#ifndef WIN32
 	}
-#endif
-	
+
+	// Check available ports.
+	displayOutputPorts(midiout);
+
 	int extraNotes = 0;
 	cout << "Extra notes to send: ";
 	cin >> extraNotes;
 	cout << endl;
 
 	// Send out a series of MIDI messages.
-
-	/*
-
-	// Program change: 192, 5
-	message.push_back( 192 );
-	message.push_back( 5 );
-	midiout->sendMessage( &message );
-
-	// Control Change: 176, 7, 100 (volume)
-	message[0] = 176;
-	message[1] = 7;
-	message.push_back( 100 );
-	midiout->sendMessage( &message );
-
-	// Note On: 144, 64, 90
-	message[0] = 144;
-	message[1] = 64;
-	message[2] = 90;
-	midiout->sendMessage( &message );
-
-	Sleep(500);
-
-	// Note Off: 128, 64, 40
-	message[0] = 128;
-	message[1] = 64;
-	message[2] = 40;
-	midiout->sendMessage( &message );
-
-	// Note On: 144, 64, 90
-	message[0] = 144;
-	message[1] = 64;
-	message[2] = 90;
-	midiout->sendMessage( &message );
-
-	Sleep(500);
-
-	// Note Off: 128, 64, 40
-	message[0] = 128;
-	message[1] = 64;
-	message[2] = 40;
-	midiout->sendMessage( &message );
-
-	// Note On: 144, 64, 90
-	message[0] = 144;
-	message[1] = 64;
-	message[2] = 90;
-	midiout->sendMessage( &message );
-
-	Sleep(500);
-
-	// Note Off: 128, 64, 40
-	message[0] = 128;
-	message[1] = 64;
-	message[2] = 40;
-	midiout->sendMessage( &message );
-
-	// Note On: 144, 64, 90
-	message[0] = 144;
-	message[1] = 64;
-	message[2] = 90;
-	midiout->sendMessage( &message );
-
-	Sleep(500);
-
-	// Note Off: 128, 64, 40
-	message[0] = 128;
-	message[1] = 64;
-	message[2] = 40;
-	midiout->sendMessage( &message );
-
-	// Note On: 144, 64, 90
-	message[0] = 144;
-	message[1] = 64;
-	message[2] = 90;
-	midiout->sendMessage( &message );
-
-	Sleep(250);
-
-	// Note Off: 128, 64, 40
-	message[0] = 128;
-	message[1] = 64;
-	message[2] = 40;
-	midiout->sendMessage( &message );
-
-	// Note On: 144, 64, 90
-	message[0] = 144;
-	message[1] = 64;
-	message[2] = 90;
-	midiout->sendMessage( &message );
-
-	Sleep(250);
-
-	// Note Off: 128, 64, 40
-	message[0] = 128;
-	message[1] = 64;
-	message[2] = 40;
-	midiout->sendMessage( &message );
-
-	// Note On: 144, 64, 90
-	message[0] = 144;
-	message[1] = 64;
-	message[2] = 90;
-	midiout->sendMessage( &message );
-
-	Sleep(500);
-
-	// Note Off: 128, 64, 40
-	message[0] = 128;
-	message[1] = 64;
-	message[2] = 40;
-	midiout->sendMessage( &message );
-
-	*/
-
 	std::vector<unsigned int> gamme;
 	gamme.push_back(0);
 	gamme.push_back(3);
@@ -266,6 +161,7 @@ int main()
 	b = (rand() % 4 + 4);
 	c = rand() % 128;
 	std::cout << "a: " << a << ", b: " << b << ", c: " << c << std::endl;
+
 	// Program change: 192, 5
 	message.resize(2);
 	message[0] = 192;
