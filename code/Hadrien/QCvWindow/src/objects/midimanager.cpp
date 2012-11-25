60804dffadfd6adff49c943baa9e05e5879209f3
vanTot <tot.c.moi@gmail.com>
2012-11-25 23:32:38 +0100

    Ajout du VirtualMIDISDK et modification du midimanager

--- code/Hadrien/QCvWindow/src/objects/midimanager.cpp
+++ code/Hadrien/QCvWindow/src/objects/midimanager.cpp
@@ -4,7 +4,7 @@
 
 MidiManager::~MidiManager() throw()
 {
-	
+
 }
 
 void MidiManager::createPort(QString name)
@@ -15,6 +15,24 @@ void MidiManager::createPort(QString name)
 
 #ifdef WIN32
 	// Go VirtuamMIDISDK
+	// Pour ça il faut ajouter le teVirtualMIDI.h, ainsi que compiler avec teVirtualMIDI32.lib
+
+	// On a besoin du nom de port en LPCWSTR, d'où le code un peu dégueu qui va suivre
+    int len;
+    int slength = (int)name.length() + 1;
+    len = MultiByteToWideChar(CP_ACP, 0, name.c_str(), slength, 0, 0);
+    wchar_t* buf = new wchar_t[len];
+    MultiByteToWideChar(CP_ACP, 0, name.c_str(), slength, buf, len);
+    std::wstring r(buf);
+    delete[] buf;
+
+    port = virtualMIDICreatePortEx2(r.c_str, teVMCallback, 0, MAX_SYSEX_BUFFER, TE_VM_FLAGS_PARSE_RX );
+	if ( !port ) {
+		printf( "could not create port: %d\n", GetLastError() );
+	}
+    // A ce moment là, le port est créé et il est possible de s'y connecter
+
+	// Il faut ajouter "if(port != NULL) virtualMIDIClosePort( port );" dans le destructeur
 #endif
 }
 
@@ -26,7 +44,7 @@ void MidiManager::changePort(int n)
 void MidiManager::sendMessage(const unsigned char* msg)
 {
 	std::vector <unsigned char> v;
-	
+
 	if(((msg[0] & 0xf0) == 0xc0) || ((msg[0] & 0xf0) == 0xd0))//program change or channel aftertouch
 	{
 		qDebug() << "size 2 : program change or channel aftertouch";
--- code/Hadrien/QCvWindow/src/objects/midimanager.h
+++ code/Hadrien/QCvWindow/src/objects/midimanager.h
@@ -9,15 +9,17 @@
 class MidiManager : public QObject, public RtMidiOut
 {
 	Q_OBJECT
-	
+
+	private LPVM_MIDI_PORT port; // L'initialiser à NULL dans le constructeur
+
 	public:
 		~MidiManager() throw();
-	
+
 	public slots:
 		void createPort(QString name);
 		void changePort(int n);
 		void sendMessage(const unsigned char* msg);
-	
+
 };
 
 #endif //MIDIMANAGER_H
